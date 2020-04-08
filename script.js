b892c9938981d60e7f751407098a6206ba2a1308
Will Rowston <willrowston@gmail.com>
2020-04-08 17:57:09 -0400

    linked the appropriate image to each card

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-function createDeck() {
-    cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'Ace']
-    cardSuit = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
-    deck = []
+const cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'Ace']
+const cardSuit = ['hearts', 'spades', 'clubs', 'diamonds']
+let deck = []
 
-    for (let i = 0; i < cardSuit.length; i++) {
-        for (let j = 0; j < cardValue.length; j++) {
-            deck.push(cardValue[j] + ' of ' + cardSuit[i])
-        }
+for (let i = 0; i < cardSuit.length; i++) {
+    for (let j = 0; j < cardValue.length; j++) {
+        let card = {}
+        card.value = cardValue[j]
+        card.suit = cardSuit[i]
+        card.image = `card-images/${cardValue[j]}_of_${cardSuit[i]}.png`
+        deck.push(card)
     }
-    console.log(deck)
 }
-createDeck()
+console.log(deck[17].image)
+console.log(deck)
+
+let cardElement = document.createElement('img')
+cardElement.setAttribute('src', deck[17].image)
+document.body.appendChild(cardElement)
 
