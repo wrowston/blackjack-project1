d031f7b057fbf1db3add909c12e598071f2d7cf5
Will Rowston <willrowston@gmail.com>
2020-04-09 10:34:46 -0400

    added function to shuffle cards

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const cardValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'Ace']
 const cardSuit = ['hearts', 'spades', 'clubs', 'diamonds']
 let deck = []
+let user = {
+    cards: [],
+    score: 0
+}
+let dealer = {
+    cards: [],
+    score: 0
+}
 
-for (let i = 0; i < cardSuit.length; i++) {
-    for (let j = 0; j < cardValue.length; j++) {
-        let card = {}
-        card.value = cardValue[j]
-        card.suit = cardSuit[i]
-        card.image = `card-images/${cardValue[j]}_of_${cardSuit[i]}.png`
-        deck.push(card)
+function createDeck() {
+    for (let i = 0; i < cardSuit.length; i++) {
+        for (let j = 0; j < cardValue.length; j++) {
+            let card = {}
+            card.value = cardValue[j]
+            card.suit = cardSuit[i]
+            card.image = `images/card-images/${cardValue[j]}_of_${cardSuit[i]}.png`
+            deck.push(card)
+        }
     }
 }
-console.log(deck[17].image)
-console.log(deck)
 
-let cardElement = document.createElement('img')
-cardElement.setAttribute('src', deck[17].image)
-document.body.appendChild(cardElement)
+createDeck()
+// console.log(deck[17].image)
+// console.log(deck)
 
+// let cardElement = document.createElement('img')
+// cardElement.setAttribute('src', deck[17].image)
+// document.body.appendChild(cardElement)
+
+function shuffleCards(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let randomIndex = Math.floor(Math.random() * deck.length)
+
+        let tempValue = deck[i] 
+        deck[i] = deck[randomIndex]
+        deck[randomIndex] = tempValue
+    }
+    return deck
+}
