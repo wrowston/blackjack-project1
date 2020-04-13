508962ec10251e0d922c75f54cb42d683cd9646d
Will Rowston <willrowston@gmail.com>
2020-04-13 12:29:07 -0400

    styled buttons with bootstrap

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ let dealer = {
     score: 0,
     wins: 0
 }
-const dealBtn = document.querySelectorAll('.btn.btn-secondary.btn-lg.deal')[0]
-const hitBtn = document.querySelectorAll('.btn.btn-secondary.btn-lg.hit')[0]
-const standBtn = document.querySelectorAll('.btn.btn-secondary.btn-lg.stand')[0]
+const dealBtn = document.querySelectorAll('.deal')[0]
+const hitBtn = document.querySelectorAll('.hit')[0]
+const standBtn = document.querySelectorAll('.stand')[0]
 
 //-------------------------------------
 //------------CARD HANDLING------------
